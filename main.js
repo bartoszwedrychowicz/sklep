255f961936a05f3cd434ff6681e6b228e587c952
bartoszwedrychowicz <bartosz.wedrychowicz@onet.pl>
2022-09-08 19:39:33 +0200

    js dalej

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
-const categoryTitle = document.querySelectorAll('.category-title');
+const categoryTitle = document.querySelectorAll(".category-title")
 console.log(categoryTitle);
+
+let licznik = null
+
+for (let i =0; i < categoryTitle.length; i++){
+    categoryTitle[i].addEventListener('click', () =>{
+        categoryTitle[i].classList.add('active')
+        licznik++
+        console.log(licznik)
+        if(licznik==1){
+            console.log("działa");
+            licznik = 0;
+        }
+    })
+}
